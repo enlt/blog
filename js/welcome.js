3711f6ff20502d3750710f55ad93eef0e7d99b3e
enlt <regretify@gmail.com>
2024-07-21 08:02:13 +0800

    Site updated: 2024-07-21 08:02:12

--- js/welcome.js
+++ js/welcome.js
@@ -29,7 +29,7 @@ function getDistance(e1, n1, e2, n2) {
 
 function showWelcome() {
 
-    let dist = getDistance(32.59944, 112.45222, ipLocation.result.location.lng, ipLocation.result.location.lat); //这里记得换成自己的经纬度
+    let dist = getDistance(112.45790949501657, 32.601018793134756, ipLocation.result.location.lng, ipLocation.result.location.lat); //这里记得换成自己的经纬度
     let pos = ipLocation.result.ad_info.nation;
     let ip;
     let posdesc;
